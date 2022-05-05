6a510d6f8866b30894382a44a8795f506da6b65a
Deepesh Choudhary <dcdeepesh7@gmail.com>
2022-05-05 14:04:02 +0530

    Position settings window

--- Doge/UI/SettingsWindow.xaml.cs
+++ Doge/UI/SettingsWindow.xaml.cs
@@ -6,10 +6,29 @@ namespace Doge {
         public SettingsWindow() {
             DataContext = Preferences.Current;
             InitializeComponent();
+            Loaded += (sender, args) => PositionWindow();
         }
 
         private void OnWindowClosed(object sender, EventArgs e) {
             Preferences.Save();
+        }
+
+        private void PositionWindow() {
+            int margin = 0;
+            var workArea = SystemParameters.WorkArea;
+            if (workArea.Top != 0) {
+                // taskbar is on the top
+                Top = workArea.Top + margin;
+                Left = workArea.Right - ActualWidth - margin;
+            } else if (workArea.Left != 0) {
+                // taskbar is on the left
+                Top = workArea.Bottom - ActualHeight - margin;
+                Left = workArea.Left + margin;
+            } else {
+                // tqaskbar is on the right or bottom
+                Top = workArea.Bottom - ActualHeight - margin;
+                Left = workArea.Right - ActualWidth - margin;
+            }
         }
     }
 }
