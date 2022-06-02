0580c06b0fd2b0bcd7d0c6b7e91cc2ba7933e9a1
Deepesh Choudhary <dcdeepesh7@gmail.com>
2022-06-03 01:37:37 +0530

    Hide overlay from Alt+Tab switcher

--- Doge/UI/OverlayWindow.xaml.cs
+++ Doge/UI/OverlayWindow.xaml.cs
@@ -1,10 +1,26 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace Doge {
     public partial class OverlayWindow : Window {
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr window, int index);
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr window, int index, int value);
+
         public OverlayWindow() {
             InitializeComponent();
             DataContext = Preferences.Current;
+            Loaded += (_, _) => {
+                const int GWL_EXSTYLE = -20;
+                const int WS_EX_TOOLWINDOW = 0x00000080;
+
+                var handle = new WindowInteropHelper(this).Handle;
+                int currentStyle = GetWindowLong(handle, GWL_EXSTYLE);
+                SetWindowLong(handle, GWL_EXSTYLE, currentStyle | WS_EX_TOOLWINDOW);
+            };
         }
     }
 }
