c9a46644a50c2a39e21248d5c98fbaf03e623618
Deepesh Choudhary <dcdeepesh7@gmail.com>
2022-06-28 10:58:29 +0530

    Remove false copyright

--- Doge/Properties/AssemblyInfo.cs
+++ Doge/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Doge")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
